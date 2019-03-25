aeb7576d9756cd44b2ac2282caaeacc24a06da84
David <david.yang026@gmail.com>
2019-03-25 16:14:07 -0700

    added malfunctions

--- combine.py
+++ combine.py
@@ -21,6 +21,42 @@ def feature_addition(df):
 
     df['trip_date'] = df.start_time.dt.date
 
+    #any trip with less than 90 seconds and where
+    #start station IS the end station,
+    #we make the assumption that its malfunctioned
+    df['malfunction'] = (df.duration_sec < 90) & 
+                        (df.start_station_name == df.end_station_name)
+
     return df
+
+def num_malfunctions(df):
+    '''
+    INPUT: Dataframe with a "malfunction" column
+    Sum the number of malfunctions up
+    OUTPUT: Tuple with 
+            first element as number of malfunctions and
+            second element as number of non-malfunctions
+    '''
+
+    num_malfunctions = df.malfunction.sum()
+    num_working = len(df.malfunction) - num_malfunctions
+
+    return (num_malfunctions, num_working)
+
+def frequent_malfunction(df):
+    return df.bike_id[df.malfunction == True].value_counts()
+
+def same_station(df):
+    return df.bike_id[df.start_station_name == df.end_station_name].value_counts()
+
+def trips_weather_combined(df1, df2):
+    '''
+    INPUT df1 is the trips dataframe
+          df2 is the weather dataframe
+    OUTPUT 1 combined dataframe
+    '''
+
+
+
 
 
