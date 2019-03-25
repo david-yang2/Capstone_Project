b9b65e084fde3e5f372c3f7fb272adab231f8dea
David <david.yang026@gmail.com>
2019-03-25 15:24:48 -0700

    getting started

---
+++ combine.py
@@ -0,0 +1,26 @@
+import pandas as import pd
+import matplotlib.pyplot as plt
+
+
+
+
+
+def feature_addition(df):
+    '''
+    INPUT dataframe
+    OUTPUT dataframe with extra features
+    '''
+    #create a new column that shows the day of week for each trip
+    #need to transform panda series to datetime
+    #before we can use .dt.dayofweek
+    #monday = 0 and sunday = 6
+
+    df.start_time = pd.to_datetime(df.start_time)
+
+    df['day_of_week'] = df.start_time.dt.dayofweek
+
+    df['trip_date'] = df.start_time.dt.date
+
+    return df
+
+
