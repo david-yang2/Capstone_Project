20915b6e67ef6bffdf650f4bf10a2e42fd6a0d43
David <david.yang026@gmail.com>
2019-03-26 15:40:51 -0700

    merged dfs

--- combine.py
+++ combine.py
@@ -1,9 +1,10 @@
-import pandas as import pd
+import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 
-
-
+trips = pd.read_csv("data/201812-fordgobike-tripdata.csv")
+weather = pd.read_csv('data/weather.csv')
 
 def feature_addition(df):
     '''
@@ -19,13 +20,12 @@ def feature_addition(df):
 
     df['day_of_week'] = df.start_time.dt.dayofweek
 
-    df['trip_date'] = df.start_time.dt.date
+    df['date'] = df.start_time.dt.date
 
     #any trip with less than 90 seconds and where
     #start station IS the end station,
     #we make the assumption that its malfunctioned
-    df['malfunction'] = (df.duration_sec < 90) & 
-                        (df.start_station_name == df.end_station_name)
+    df['malfunction'] = (df.duration_sec < 90) & (df.start_station_name == df.end_station_name)
     df['age'] = 2019 - df.member_birth_year
 
     return df
@@ -51,6 +51,16 @@ def same_station(df):
     return df.bike_id[df.start_station_name == df.end_station_name].value_counts()
 
 def model_city(df, city = 1):
+    '''
+    Breaks the dataset into 3 cities
+    SF = 1
+    OAK = 2
+    SJ = 3
+
+    INPUT: Dataframe
+           Number for city to be modeled
+    OUTPUT: returns a portion of the original dataframe
+    ''' 
     if city == 1:
         return df[(df.start_station_latitude > 37.697799) & (df.start_station_longitude <-122.330676)]
     elif city == 2:
@@ -65,7 +75,20 @@ def trips_weather_combined(df1, df2):
     OUTPUT 1 combined dataframe
     '''
 
+def get_dummies(df):
+    
+    #start station dummies
+    start_dummies = pd.get_dummies(df.start_station_name)
+    #end station dummies
+    end_dummies = pd.get_dummies(df.end_station_name)
+    df = pd.concat([df,start_dummies], axis=1)
+    df = pd.concat([df,end_dummies], axis=1)
+    
+    return df
 
-
-
+def merge_dfs(df1, df2, column = 'date'):
+    df2[column] = pd.to_datetime(df2[column])
+    df2[column] = df2[column].dt.date
+    combined_df = pd.merge(df1,df2, on='date', how='left')
+    return combined_df
 
